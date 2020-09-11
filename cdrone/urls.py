0d22963a82ab612f6f44bd9ed5c350b72f092282
ismewen <ismewen@outlook.com>
2020-09-11 12:32:20 +0800

    expired time

--- cdrone/urls.py
+++ cdrone/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 import apps.helloworld.urls
 
+
+def index(request):
+    return HttpResponse("index")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
 
 urlpatterns = urlpatterns + apps.helloworld.urls.urlpatterns
