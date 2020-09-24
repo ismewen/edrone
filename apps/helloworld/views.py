00e63282a1787d1b305612575ab718dc22e1daed
ismewen <ismewen@outlook.com>
2020-09-24 16:12:49 +0800

    edrone init

--- apps/helloworld/views.py
+++ apps/helloworld/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -11,3 +12,14 @@ logger = logging.getLogger("default")
 def hello_world(requests):
     logger.info("hello world")
     return HttpResponse("hello world")
+
+
+class EdroneAPIView(APIView):
+
+    def get(self, *args, **kwargs):
+        logger.info(self.request.data)
+        return HttpResponse("hello")
+
+    def post(self, *args, **kwargs):
+        logger.info(self.request.data)
+        return HttpResponse("hello")
