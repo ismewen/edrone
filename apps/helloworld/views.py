f4be464d1ead879a08d7a35949277890c2c1a9bc
ismewen <ismewen@outlook.com>
2020-09-25 19:48:41 +0800

    update requirement.txt

--- apps/helloworld/views.py
+++ apps/helloworld/views.py
@@ -8,6 +8,61 @@ from rest_framework.views import APIView
 
 logger = logging.getLogger("default")
 
+yaml = """
+kind: pipeline
+name: default
+
+steps:
+  - name: docker
+    image: plugins/docker
+    settings:
+      repo: fscripy/edrone
+      username:
+        from_secret: fscripyu_username
+      password:
+        from_secret: fscripyu_password
+    volumes:
+      - name: dockercache
+        path: /var/lib/docker
+#  - name: deploy
+#    image: pelotech/drone-helm3
+#    settings:
+#      api_server:
+#        from_secret: k8s_api_server
+#      kubernetes_token:
+#        from_secret: k8s_token
+#      kube_certificate:
+#        from_secret: k8s_ca
+#      chart: ./edrone
+#      release: edrone
+#      helm_command: upgrade
+#      wait: true
+#      force: true
+#      namespace: edrone
+
+  - name: deploy
+    image: bitsbeats/drone-helm3
+    settings:
+      kube_api_server:
+        from_secret: k8s_api_server
+      kube_token:
+        from_secret: k8s_token
+      kube_certificate:
+        from_secret: k8s_ca
+      chart: ./edrone
+      release: edrone
+      helm_command: upgrade
+      wait: true
+      force: false
+      namespace: default
+      vaules_yaml: ["./edrone/values.yaml"]
+
+volumes:
+- name: dockercache
+  host:
+    path: /tmp/docker/${DRONE_REPO}/${DRONE_BRANCH}
+"""
+
 
 def hello_world(requests):
     logger.info("hello world")
@@ -19,11 +74,6 @@ class EdroneAPIView(APIView):
 
     def post(self, *args, **kwargs):
         logger.info(self.request.data)
-        yaml = """
-        a: hello
-        b: world 
-        """
-        media_type = 'application/json'
         return HttpResponse(yaml)
 
     get = post
